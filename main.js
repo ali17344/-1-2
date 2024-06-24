1bad17f41cd1ffb9a87610ab8d5fa89065b89731
ali17344 <elokctr96@gmail.com>
2024-06-24 22:59:46 +0600

    intital

---
+++ main.js
@@ -0,0 +1,16 @@
+let num = 9;
+num = 80.5;
+if(num % 2 == 0){
+    console.log("Чётное");
+}else if(num % 2 == 1){
+    console.log("Нечётное")
+} else  {
+console.log("Введите только числа")
+}
+num = -9.689;
+if(num < 0){
+    console.log("Отрицательный");
+}else if(num > 0 ){
+    console.log("Положительный")
+} 
+
